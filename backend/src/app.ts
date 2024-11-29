7b28885777055de6d279b23601715743669a9a4a
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-11-29 12:23:55 +0100

    issue might be resolved

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://chappy-frontend.onrender.com'
+    'https://chappy-frontend.onrender.com',
+    'https://chappyv.onrender.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
